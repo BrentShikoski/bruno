f48261b5fb5d34a544cd981e6f262595f43a10a1
Brent Shikoski <brent@shikoski.com>
2023-09-15 22:59:30 -0500

    build windows dont sign

--- packages/bruno-electron/electron-builder-config.js
+++ packages/bruno-electron/electron-builder-config.js
@@ -12,44 +12,9 @@ const config = {
     "**/*"
   ],
   "afterSign": "notarize.js",
-  "mac": {
-    "artifactName": "${name}_${version}_${arch}_${os}.${ext}",
-    "category": "public.app-category.developer-tools",
-    "target": [
-      {
-        "target": "dmg",
-        "arch": [
-          "x64",
-          "arm64"
-        ]
-      },
-      {
-        "target": "zip",
-        "arch": [
-          "x64",
-          "arm64"
-        ]
-      }
-    ],
-    "icon": "resources/icons/mac/icon.icns",
-    "hardenedRuntime": true,
-    "identity": "Anoop MD (W7LPPWA48L)",
-    "entitlements": "resources/entitlements.mac.plist",
-    "entitlementsInherit": "resources/entitlements.mac.plist"
-  },
-  "linux": {
-    "artifactName": "${name}_${version}_${arch}_linux.${ext}",
-    "icon": "resources/icons/png",
-    "target": [
-      "AppImage",
-      "deb"
-    ]
-  },
   "win": {
     "artifactName": "${name}_${version}_${arch}_win.${ext}",
-    "icon": "resources/icons/png",
-    "certificateFile": `${process.env.WIN_CERT_FILEPATH}`,
-    "certificatePassword": `${process.env.WIN_CERT_PASSWORD}`,
+    "icon": "resources/icons/png"
   }
 };
 
